424c2aa681916631340a142719ab2c23e2559e12
gsujanth <princ@DREAMCHASER>
2019-04-09 21:41:15 -0400

    refactored

--- src/main/java/com/sujanth/logindemo1/LoginDemoApplication.java
+++ src/main/java/com/sujanth/logindemo1/LoginDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LoginDemo1Application {
+public class LoginDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LoginDemo1Application.class, args);
+		SpringApplication.run(LoginDemoApplication.class, args);
 	}
 
 }
--- src/test/java/com/sujanth/logindemo1/LoginDemoApplicationTests.java
+++ src/test/java/com/sujanth/logindemo1/LoginDemoApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class LoginDemo1ApplicationTests {
+public class LoginDemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
